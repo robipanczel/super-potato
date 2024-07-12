79cafe5be97abe523c46e3b520af8c90e73991d7
Róbert Pánczél <robi.panczel@gmail.com>
2024-07-12 17:01:19 +0200

    Separator handling

--- cmd/clone.go
+++ cmd/clone.go
@@ -18,75 +18,73 @@ import (
 // cloneCmd represents the clone command
 var cloneCmd = &cobra.Command{
 	Use:   "clone",
-	Short: "Clone source dashboard and create copies with featureId prefix",
-	Long: `Download dashboard based on the given source URL
+	Short: "Clone dashboard and create copies with featureId prefix",
+	Long: `Download dashboard based on the given URL
 	and create copies of it with the featureId prefix in the name
-	Ideally the source dashboard should be the stating dashboard.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		//1. Get dashboard/report based on name
-		//2. Create a copy dashboard in splunk
-		//2. Search for all the savedsearches in the dashboard/report
-		//3. Put the found savedsearch in an array
-		//4. Pop out the last name and start with 1. step
-
-		slog.Info("clone command initiated", "dashboard", dashboard, "featureId", featureId)
-
-		reportNames := make([]string, 0, 10)
-		reportNames = append(reportNames, dashboard)
-		processedReportCounter := 0
-
-		for len(reportNames) != processedReportCounter {
-			currentReportName := reportNames[processedReportCounter]
-			processedReportCounter = processedReportCounter + 1
-
-			xmlString, err := ReadXml(currentReportName)
-			if err != nil {
-				slog.Error("failed to load xml",
-					"dashboard", dashboard,
-					"name", currentReportName,
-					"error", err,
-				)
-				panic(err)
-			}
-
-			foundReportNames := CollectLinkedReportsFromXmlString(xmlString, dashboard, currentReportName, reportPrefix)
-			slog.Info("reports found",
-				"dashboard", dashboard,
-				"current", currentReportName,
-				"linked reports", foundReportNames,
-			)
-			reportNames = append(reportNames, foundReportNames...)
-
-			withNewPrefix := ReplaceOldPrefixWithNewPrefix(currentReportName, reportPrefix, featureId)
-			xmlString = ReplaceOldPrefixWithNewPrefixInXml(xmlString, reportPrefix, featureId, foundReportNames)
-
-			err = WriteXml(withNewPrefix, xmlString)
-			if err != nil {
-				slog.Error("failed to upload cloned xml",
-					"dashboard", dashboard,
-					"current", currentReportName,
-					"error", err,
-				)
-				panic(err)
-			}
-
-		}
-		slog.Info("clone command done", "dashboard", dashboard, "featureId", featureId, "generated reports", reportNames)
-	},
+	Ideally the dashboard should be the stating dashboard.`,
+	Run: runClone,
 }
 
 var dashboard string
 var reportPrefix string
 var featureId string
+var separator string
 
 func init() {
-	cloneCmd.Flags().StringVarP(&dashboard, "dashboard", "d", "", "URL of the source dashboard from which to clone from (required)")
-	cloneCmd.Flags().StringVarP(&reportPrefix, "reportPrefix", "r", "", "Prefix of the source dashboard and its reports")
-	cloneCmd.Flags().StringVarP(&featureId, "featureId", "f", "", "ID of the feature that the clone dashboard is for (required)")
+	cloneCmd.Flags().StringVarP(&dashboard, "dashboard", "d", "", "URL of the dashboard from which to clone from (required)")
+	cloneCmd.Flags().StringVarP(&reportPrefix, "reportPrefix", "r", "", "Prefix of the dashboard and its reports")
+	cloneCmd.Flags().StringVarP(&featureId, "featureId", "f", "", "ID of the feature that the clone dashboard is for")
 	cloneCmd.MarkFlagRequired("dashboard")
-	cloneCmd.MarkFlagRequired("featureId")
+
+	separator = "_"
 
 	rootCmd.AddCommand(cloneCmd)
+}
+
+func runClone(cmd *cobra.Command, args []string) {
+	slog.Info("clone command initiated", "dashboard", dashboard, "featureId", featureId)
+
+	reportNames := make([]string, 0, 10)
+	reportNames = append(reportNames, dashboard)
+	processedReportCounter := 0
+
+	for len(reportNames) != processedReportCounter {
+		currentReportName := reportNames[processedReportCounter]
+		processedReportCounter = processedReportCounter + 1
+
+		xmlString, err := ReadXml(currentReportName)
+		if err != nil {
+			slog.Error("failed to load xml",
+				"dashboard", dashboard,
+				"name", currentReportName,
+				"error", err,
+			)
+			panic(err)
+		}
+
+		foundReportNames := CollectLinkedReportsFromXmlString(xmlString, dashboard, currentReportName, reportPrefix)
+		slog.Info("reports found",
+			"dashboard", dashboard,
+			"current", currentReportName,
+			"linked reports", foundReportNames,
+		)
+		reportNames = append(reportNames, foundReportNames...)
+
+		withNewPrefix := ReplaceOldPrefixWithNewPrefix(currentReportName, reportPrefix, featureId, separator)
+		xmlString = ReplaceOldPrefixWithNewPrefixInXml(xmlString, reportPrefix, featureId, separator, foundReportNames)
+
+		err = WriteXml(withNewPrefix, xmlString)
+		if err != nil {
+			slog.Error("failed to upload cloned xml",
+				"dashboard", dashboard,
+				"current", currentReportName,
+				"error", err,
+			)
+			panic(err)
+		}
+
+	}
+	slog.Info("clone command done", "dashboard", dashboard, "featureId", featureId, "generated reports", reportNames)
 }
 
 func CollectLinkedReportsFromXmlString(xmlString string, dashboard string, currentReport string, reportPrefix string) (reportNames []string) {
@@ -114,24 +112,25 @@ func CollectLinkedReportsFromXmlString(xmlString string, dashboard string, curre
 	return reportNames
 }
 
-func ReplaceOldPrefixWithNewPrefixInXml(xmlString, oldPrefix, newPrefix string, reportNames []string) string {
+func ReplaceOldPrefixWithNewPrefixInXml(xmlString, oldPrefix, newPrefix, separator string, reportNames []string) string {
 	for _, foundReportName := range reportNames {
-		withNewPrefix := ReplaceOldPrefixWithNewPrefix(foundReportName, oldPrefix, newPrefix)
+		withNewPrefix := ReplaceOldPrefixWithNewPrefix(foundReportName, oldPrefix, newPrefix, separator)
 		xmlString = strings.ReplaceAll(xmlString, foundReportName, withNewPrefix)
 	}
 	return xmlString
 }
 
-func ReplaceOldPrefixWithNewPrefix(foundReportName, oldPrefix, newPrefix string) string {
+func ReplaceOldPrefixWithNewPrefix(foundReportName, oldPrefix, newPrefix, separator string) string {
 	withoutPrefix := strings.TrimPrefix(foundReportName, oldPrefix)
-	withNewPrefix := fmt.Sprintf("%s_%s", newPrefix, withoutPrefix)
+	withoutPrefixAndSeparator := strings.TrimPrefix(withoutPrefix, separator)
+	withNewPrefix := fmt.Sprintf("%s%s%s", newPrefix, separator, withoutPrefixAndSeparator)
 	return withNewPrefix
 }
 
 func ReadXml(name string) (xmlString string, err error) {
-	data, err := os.ReadFile(filepath.Join(".", "cmd", "tests", name))
+	data, err := os.ReadFile(filepath.Join(".", "tests", name))
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	return string(data), nil
@@ -140,7 +139,7 @@ func ReadXml(name string) (xmlString string, err error) {
 func WriteXml(name string, xml string) error {
 	err := os.WriteFile(filepath.Join("temp", name), []byte(xml), os.ModePerm)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	return nil
 }
--- cmd/clone_test.go
+++ cmd/clone_test.go
@@ -1,9 +1,13 @@
 package cmd
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 func TestReadXml_ExistingXml(t *testing.T) {
-	data, err := ReadXml("splunk_example_1.xml")
+	data, err := ReadXml("splunk_example_1")
 
 	if err != nil {
 		t.Errorf("should have read the file, but found: %s", err)
@@ -13,9 +17,86 @@ func TestReadXml_ExistingXml(t *testing.T) {
 		t.Errorf("parsed string xml should be longer then 0 length")
 	}
 }
+
 func TestReadXml_NotExistingXml(t *testing.T) {
-	_, err := ReadXml("foo.xml")
+	_, err := ReadXml("barfoo")
 	if err == nil {
 		t.Errorf("should have thrown an error if file does not exits")
+	}
+}
+
+func A() {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(path)
+}
+
+func TestReplaceOldPrefixWithNewPrefix(t *testing.T) {
+	separator := "_"
+
+	testCases := []struct {
+		foundReportName string
+		oldPrefix       string
+		newPrefix       string
+		expectedResult  string
+	}{
+		{
+			foundReportName: "dashboard_name",
+			oldPrefix:       "prod",
+			newPrefix:       "dev",
+			expectedResult:  "dev_dashboard_name",
+		},
+		{
+			foundReportName: "prod_dashboard_name",
+			oldPrefix:       "prod_",
+			newPrefix:       "dev_",
+			expectedResult:  "dev__dashboard_name",
+		},
+		{
+			foundReportName: "dev_dashboard_name",
+			oldPrefix:       "prod",
+			newPrefix:       "dev",
+			expectedResult:  "dev_dev_dashboard_name",
+		},
+	}
+
+	for _, testCase := range testCases {
+		result := ReplaceOldPrefixWithNewPrefix(testCase.foundReportName, testCase.oldPrefix, testCase.newPrefix, separator)
+		if result != testCase.expectedResult {
+			t.Errorf("ReplaceOldPrefixWithNewPrefix(%s, %s, %s) = %s, expected %s",
+				testCase.foundReportName, testCase.oldPrefix, testCase.newPrefix, result, testCase.expectedResult)
+		}
+	}
+}
+
+func TestReplaceOldPrefixWithNewPrefixInXml(t *testing.T) {
+	separator := "_"
+	xmlString := `
+<dashboard>
+	<panel>
+		<savedsearch>prod_dashboard_name</savedsearch>
+		<savedsearch>prod_report_1</savedsearch>
+		<savedsearch>prodreport_2</savedsearch>
+	</panel>
+</dashboard>
+`
+	oldPrefix := "prod"
+	newPrefix := "dev"
+	reportNames := []string{"prod_dashboard_name", "prod_report_1", "prodreport_2"}
+	expectedResult := `
+<dashboard>
+	<panel>
+		<savedsearch>dev_dashboard_name</savedsearch>
+		<savedsearch>dev_report_1</savedsearch>
+		<savedsearch>dev_report_2</savedsearch>
+	</panel>
+</dashboard>
+`
+
+	result := ReplaceOldPrefixWithNewPrefixInXml(xmlString, oldPrefix, newPrefix, separator, reportNames)
+	if result != expectedResult {
+		t.Errorf("ReplaceOldPrefixWithNewPrefixInXml() = %s, expected %s", result, expectedResult)
 	}
 }
